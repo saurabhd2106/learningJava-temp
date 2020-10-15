5fd98869376b1319b5080834614ce44775845415
=Saurabh <=>
2020-10-15 06:36:10 +0000

    First cli commit

---
+++ LearningJava/src/day1/HelloWorld.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		System.out.println("Hello World Again");
+		
+		System.out.println("Hello World Again");
+
+	}
+
+}
